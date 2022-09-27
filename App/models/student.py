3d87fd5db4ede5bd410a5ec7ee6a3ce3954f8249
satyaan <satyaandm@gmail.com>
2022-09-26 23:32:02 -0400

    student class

---
+++ App/models/student.py
@@ -0,0 +1,24 @@
+from App.database import db
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    faculty = db.Column(db.String, nullable=True)
+    programme = db.Column(db.String, nullable=True)
+    karma = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+        self.karma = 0
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'student_id': self.student_id,
+            'name': self.name,
+            'faculty': self.faculty,
+            'programme': self.programme,
+            'karma': self.karma
+        }
