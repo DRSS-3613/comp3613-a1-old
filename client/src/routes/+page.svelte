0ea4f2cb37ada859cfd4a13b3564952e0a761dc6
Daniel Yatali <danielyatali902@gmail.com>
2022-09-27 19:33:02 -0400

    Setup login and signup pages

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1 class="text-red-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Nav from '../components/nav.svelte';
+</script>
+
+<div class="bg-rural-blue h-28">
+    <Nav />
+</div>
+<div class="w-full h-full flex flex-col items-center">
+    <div class="h-52"></div>
+    <div>
+        Student Conduct Tracker
+    </div>
+</div>
+
+<svelte:head>
+	<title>Home</title>
+	<link rel="icon" href="https://i.imgur.com/0CUwW1S.jpeg" />
+	<style>
+		body {
+			background-color: #f4f4f3;
+		}
+	</style>
+</svelte:head>
