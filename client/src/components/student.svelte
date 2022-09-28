67a8767bf178077cf8d7707718655dcf1a5924bf
Daniel Yatali <danielyatali902@gmail.com>
2022-09-27 22:08:25 -0400

    added dashboard

---
+++ client/src/components/student.svelte
@@ -0,0 +1,30 @@
+	 <div class="main">
+        <!--card-->
+        <div class="card bg-white flex flex-col items-center justify-center p-4 shadow-lg rounded-2xl w-64">
+            <!--profile-image-->
+            
+            <div class="image rounded-full p-2 shadow-2xl">
+      <div class="profile mx-auto rounded-full w-24 text-blue-600">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+</svg>
+    </div>
+            </div>
+            <!--name-->
+            <div class="name text-gray-800 text-2xl font-medium mt-4">
+                <p>Daniel Yatali</p>
+            </div>
+            <!--username-->
+            <div class="username text-gray-500">
+                <p>816024289</p>
+            </div>
+            <!--work-->
+            <div class="work text-gray-700 mt-4">
+                <p>Computer Science Special</p>
+            </div>
+            <!-- follow button -->
+            <div class="w-full mt-8">
+                <button class="bg-blue-500 py-2 px-4 hover:bg-blue-600 text-white w-full font-semibold rounded-lg shadow-lg">Karma 7</button>
+            </div>
+        </div>
+    </div>
