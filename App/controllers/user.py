afca3db7edfa836a463f2fac20dd0cc883a24369
satyaan <satyaandm@gmail.com>
2022-10-06 19:35:14 -0400

    admin and reviewer user functions in controller

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -2,46 +2,88 @@ from App.models import Reviewer, Admin
 from App.database import db
 
 
+# reviewer functions
 def create_reviewer(username, password):
-    newReviewer = Reviewer(username=username, password=password)
-    db.session.add(newReviewer)
+    new_reviewer = Reviewer(username=username, password=password)
+    db.session.add(new_reviewer)
     db.session.commit()
-    return newReviewer
+    return new_reviewer
 
 
-def create_admin(username, password):
-    newAdmin = Admin(username=username, password=password)
-    db.session.add(newAdmin)
-    db.session.commit()
-    return newAdmin
-
-
-# Leaving Admin out of the scope for now because I'm not sure what's the right way to do it
-def get_user_by_username(username):
-    user = Reviewer.query.filter_by(username=username).first()
-    return user
-
-
-def get_user(id):
+def get_reviewer_by_id(id):
     return Reviewer.query.get(id)
 
 
-def get_all_users():
+def get_reviewer_by_username(username):
+    return Reviewer.query.filter_by(username=username).first()
+
+
+def get_all_reviewers():
     return Reviewer.query.all()
 
 
+def get_all_reviewers_json():
+    reviewers = Reviewer.query.all()
+    if not reviewers:
+        return []
+    reviewers = [reviewer.toJSON() for reviewer in reviewers]
+    return reviewers
+
+
+def update_reviewer(id, username):
+    reviewer = get_reviewer_by_id(id)
+    if reviewer:
+        reviewer.username = username
+        db.session.add(reviewer)
+        return db.session.commit()
+    return None
+
+
+# admin functions
+def create_admin(username, password):
+    new_admin = Admin(username=username, password=password)
+    db.session.add(new_admin)
+    db.session.commit()
+    return new_admin
+
+
+def get_admin_by_id(id):
+    return Admin.query.get(id)
+
+
+def get_admin_by_username(username):
+    return Admin.query.filter_by(username=username).first()
+
+
+def get_all_admins():
+    return Admin.query.all()
+
+
+def get_all_admins_json():
+    admins = Admin.query.all()
+    if not admins:
+        return []
+    admins = [admin.toJSON() for admin in admins]
+    return admins
+
+
+def update_admin(id, username):
+    admin = get_admin_by_id(id)
+    if admin:
+        admin.username = username
+        db.session.add(admin)
+        return db.session.commit()
+    return None
+
+
+# all users
+def get_all_users():
+    return Reviewer.query.all() + Admin.query.all()
+
+
 def get_all_users_json():
-    users = Reviewer.query.all()
+    users = get_all_users()
     if not users:
         return []
     users = [user.toJSON() for user in users]
     return users
-
-
-def update_user(id, username):
-    user = get_user(id)
-    if user:
-        user.username = username
-        db.session.add(user)
-        return db.session.commit()
-    return None
